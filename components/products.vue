c9b8ff484fb0e3624064779ca5395a57ff3a7b97
mufa2906 <m.farhan2906@gmail.com>
2023-05-11 12:53:23 +0700

    menambahkan fitur search berdasarkan category

--- components/products.vue
+++ components/products.vue
@@ -15,8 +15,10 @@
             </template>
 
             <v-list>
-              <v-list-item-group>
-                <v-list-item v-for="(category,i) in categories" :key="i">
+              <v-list-item-group v-model="categoryId">
+                <v-list-item v-for="(category,i) in categories" :key="i" 
+                :value="category.id"
+                :disabled="category.id == categoryId">
                   <v-list-item-title>
                     {{ category.title }}
                   </v-list-item-title>
@@ -27,7 +29,7 @@
         </v-col>
     </v-row>
     <v-row>
-      <v-col for v-for="(product, i) in products" :key="i" cols="2">
+      <v-col for v-for="(product, i) in filteredProducts" :key="i" cols="2">
         <v-card :title="product.title">
           <v-card-action>
             <v-img
@@ -35,7 +37,7 @@
             >
             </v-img>
           </v-card-action>
-          <v-card-text>
+          <v-card-text align="center" class="product-title">
             {{ product.title }}
           </v-card-text>
         </v-card>
@@ -48,7 +50,12 @@
 export default {
   data() {
     return {
+      categoryId : false,
       categories: [
+        {
+          id: false,
+          title: 'All' 
+        },
         {
           id: 1,
           title: 'Smartphone' 
@@ -68,7 +75,7 @@ export default {
           title: "Asus Zenfone",
           thumbnail: "asus-zenfone.png",
           price: "2000000",
-          category: 1,
+          categoryId: 1,
         },
 
         {
@@ -76,21 +83,21 @@ export default {
           title: "Canon Eos 700d",
           thumbnail: "canon-eos-700d.png",
           price: "2500000",
-          category: 2,
+          categoryId: 2,
         },
         {
           id: "3",
-          title: "Lenovo A7000",
-          thumbnail: "Lenovo-A7000.png",
+          title: "Sharp 32 LED 32LE265I",
+          thumbnail: "sharp-32-led-32LE265i.png",
           price: "8000000",
-          category: 3,
+          categoryId: 3,
         },
         {
           id: "1",
           title: "Asus Zenfone",
           thumbnail: "asus-zenfone.png",
           price: "2000000",
-          category: 1,
+          categoryId: 1,
         },
 
         {
@@ -98,21 +105,21 @@ export default {
           title: "Canon Eos 700d",
           thumbnail: "canon-eos-700d.png",
           price: "2500000",
-          category: 2,
+          categoryId: 2,
         },
         {
           id: "3",
-          title: "Lenovo A7000",
-          thumbnail: "Lenovo-A7000.png",
+          title: "Sharp 32 LED 32LE265I",
+          thumbnail: "sharp-32-led-32LE265i.png",
           price: "8000000",
-          category: 3,
+          categoryId: 3,
         },
         {
           id: "1",
           title: "Asus Zenfone",
           thumbnail: "asus-zenfone.png",
           price: "2000000",
-          category: 1,
+          categoryId: 1,
         },
 
         {
@@ -120,21 +127,21 @@ export default {
           title: "Canon Eos 700d",
           thumbnail: "canon-eos-700d.png",
           price: "2500000",
-          category: 2,
+          categoryId: 2,
         },
         {
           id: "3",
-          title: "Lenovo A7000",
-          thumbnail: "Lenovo-A7000.png",
+          title: "Sharp 32 LED 32LE265I",
+          thumbnail: "sharp-32-led-32LE265i.png",
           price: "8000000",
-          category: 3,
+          categoryId: 3,
         },
         {
           id: "1",
           title: "Asus Zenfone",
           thumbnail: "asus-zenfone.png",
           price: "2000000",
-          category: 1,
+          categoryId: 1,
         },
 
         {
@@ -142,21 +149,21 @@ export default {
           title: "Canon Eos 700d",
           thumbnail: "canon-eos-700d.png",
           price: "2500000",
-          category: 2,
+          categoryId: 2,
         },
         {
           id: "3",
-          title: "Lenovo A7000",
-          thumbnail: "Lenovo-A7000.png",
+          title: "Sharp 32 LED 32LE265I",
+          thumbnail: "sharp-32-led-32LE265i.png",
           price: "8000000",
-          category: 3,
+          categoryId: 3,
         },
         {
           id: "1",
           title: "Asus Zenfone",
           thumbnail: "asus-zenfone.png",
           price: "2000000",
-          category: 1,
+          categoryId: 1,
         },
 
         {
@@ -164,21 +171,21 @@ export default {
           title: "Canon Eos 700d",
           thumbnail: "canon-eos-700d.png",
           price: "2500000",
-          category: 2,
+          categoryId: 2,
         },
         {
           id: "3",
-          title: "Lenovo A7000",
-          thumbnail: "Lenovo-A7000.png",
+          title: "Sharp 32 LED 32LE265I",
+          thumbnail: "sharp-32-led-32LE265i.png",
           price: "8000000",
-          category: 3,
+          categoryId: 3,
         },
         {
           id: "1",
           title: "Asus Zenfone",
           thumbnail: "asus-zenfone.png",
           price: "2000000",
-          category: 1,
+          categoryId: 1,
         },
 
         {
@@ -186,21 +193,21 @@ export default {
           title: "Canon Eos 700d",
           thumbnail: "canon-eos-700d.png",
           price: "2500000",
-          category: 2,
+          categoryId: 2,
         },
         {
           id: "3",
-          title: "Lenovo A7000",
-          thumbnail: "Lenovo-A7000.png",
+          title: "Sharp 32 LED 32LE265I",
+          thumbnail: "sharp-32-led-32LE265i.png",
           price: "8000000",
-          category: 3,
+          categoryId: 3,
         },
         {
           id: "1",
           title: "Asus Zenfone",
           thumbnail: "asus-zenfone.png",
           price: "2000000",
-          category: 1,
+          categoryId: 1,
         },
 
         {
@@ -208,61 +215,32 @@ export default {
           title: "Canon Eos 700d",
           thumbnail: "canon-eos-700d.png",
           price: "2500000",
-          category: 2,
+          categoryId: 2,
         },
         {
           id: "3",
-          title: "Lenovo A7000",
-          thumbnail: "Lenovo-A7000.png",
+          title: "Sharp 32 LED 32LE265I",
+          thumbnail: "sharp-32-led-32LE265i.png",
           price: "8000000",
-          category: 3,
-        },
-        {
-          id: "1",
-          title: "Asus Zenfone",
-          thumbnail: "asus-zenfone.png",
-          price: "2000000",
-          category: 1,
-        },
-
-        {
-          id: "2",
-          title: "Canon Eos 700d",
-          thumbnail: "canon-eos-700d.png",
-          price: "2500000",
-          category: 2,
-        },
-        {
-          id: "3",
-          title: "Lenovo A7000",
-          thumbnail: "Lenovo-A7000.png",
-          price: "8000000",
-          category: 3,
-        },
-        {
-          id: "1",
-          title: "Asus Zenfone",
-          thumbnail: "asus-zenfone.png",
-          price: "2000000",
-          category: 1,
-        },
-
-        {
-          id: "2",
-          title: "Canon Eos 700d",
-          thumbnail: "canon-eos-700d.png",
-          price: "2500000",
-          category: 2,
-        },
-        {
-          id: "3",
-          title: "Lenovo A7000",
-          thumbnail: "Lenovo-A7000.png",
-          price: "8000000",
-          category: 3,
+          categoryId: 3,
         },
       ],
     };
   },
+  computed: {
+    filteredProducts() {
+      if(this.categoryId) {
+        return this.products.filter(s => s.categoryId == this.categoryId)
+      }
+      return this.products
+    }
+  }
 };
 </script>
+
+<style>
+  .product-title {
+    white-space: nowrap;
+    overflow: hidden;
+  }
+</style>
